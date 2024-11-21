2611def799d47fc8404eb4bb9c6e89e4903ae842
Valentin Pozniak <waliker448@gmail.com>
2024-11-21 14:21:15 +0200

    change user model(use email instead of username)

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,43 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 
 from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    pass
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "usable_password",
+                    "password1",
+                    "password2"
+                ),
+            },
+        ),
+    )
+
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
