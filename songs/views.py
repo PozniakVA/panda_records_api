98d11ab5e13eaa837797388a6c1a28f48b47cf00
Valentin Pozniak <waliker448@gmail.com>
2024-11-23 22:17:46 +0200

    add custom permissions for song model

--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
 
 from songs.models import Song
+from songs.permissions import IsAdminUserOrReadOnly
 from songs.serializer import SongSerializer
 
 
 class SongViewSet(viewsets.ModelViewSet):
     queryset = Song.objects.all()
+    permission_classes = [IsAdminUserOrReadOnly]
     serializer_class = SongSerializer
