f9b5d0a49a44af485e37dcfc6b49f6d351118719
Valentin Pozniak <waliker448@gmail.com>
2025-01-06 19:49:41 +0200

    add signal which deletes the photo from the database when the object is deleted in service

--- services/signals.py
+++ services/signals.py
@@ -2,10 +2,10 @@ from cloudinary.uploader import destroy
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
 
-from equipment.models import Equipment
+from services.models import Service
 
 
-@receiver(post_delete, sender=Equipment)
+@receiver(post_delete, sender=Service)
 def delete_associated_file(sender, instance, **kwargs):
     if instance.photo:
         file_url = instance.photo.url
