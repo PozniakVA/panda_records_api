6579e4605894a133f086627ac5e8314384d04915
Valentin Pozniak <waliker448@gmail.com>
2025-01-17 22:04:14 +0200

    Add signal to delete old files from cloud storage when updated with new ones

--- equipment/signals.py
+++ equipment/signals.py
@@ -1,5 +1,5 @@
 from cloudinary.uploader import destroy
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 
 from equipment.models import Equipment
@@ -10,5 +10,24 @@ def delete_associated_file(sender, instance, **kwargs):
     if instance.photo:
         file_url = instance.photo.url
         public_id = file_url.split("/v1/")[1]
+
+        destroy(public_id)
+
+
+@receiver(pre_save, sender=Equipment)
+def delete_old_file_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        old_photo = Equipment.objects.get(pk=instance.pk).photo
+    except Equipment.DoesNotExist:
+        return
+
+    new_file = instance.photo
+    if not old_photo == new_file:
+
+        old_photo_url = old_photo.url
+        public_id = old_photo_url.split("/v1/")[1]
 
         destroy(public_id)
--- videos/signals.py
+++ videos/signals.py
@@ -1,5 +1,5 @@
 from cloudinary.uploader import destroy
-from django.db.models.signals import post_delete
+from django.db.models.signals import post_delete, pre_save
 from django.dispatch import receiver
 
 from videos.models import Video
@@ -17,4 +17,32 @@ def delete_associated_file(sender, instance, **kwargs):
         file_url = instance.video_file.url
         public_id = file_url.split("/v1/")[1]
 
+        destroy(public_id, resource_type="video")
+
+
+@receiver(pre_save, sender=Video)
+def delete_old_file_on_update(sender, instance, **kwargs):
+    if not instance.pk:
+        return
+
+    try:
+        video = Video.objects.get(pk=instance.pk)
+        old_poster = video.poster
+        old_video_file = video.video_file
+
+    except Video.DoesNotExist:
+        return
+
+    new_poster = instance.poster
+    if not old_poster == new_poster:
+
+        old_poster_url = old_poster.url
+        public_id = old_poster_url.split("/v1/")[1]
+
+        destroy(public_id)
+
+    new_video_file = instance.video_file
+    if not old_video_file == new_video_file:
+        old_video_file_url = old_video_file.url
+        public_id = old_video_file_url.split("/v1/")[1]
         destroy(public_id, resource_type="video")
