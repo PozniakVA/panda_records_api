1d0908d5c92add7dec20bdf0dd94ea050c1e41c1
Valentin Pozniak <waliker448@gmail.com>
2025-02-02 16:44:56 +0200

    added validation for file size

--- songs/serializer.py
+++ songs/serializer.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 from songs.models import Song
 
@@ -15,3 +16,13 @@ class SongSerializer(serializers.ModelSerializer):
             "duration",
             "top"
         ]
+
+    def validate_audio_file(self, value):
+        if value and value.size > 10 * 1024 * 1024:  # 10 MB
+            raise ValidationError("File size must not be greater than 10 MB.")
+        return value
+
+    def validate_photo(self, value):
+        if value and value.size > 10 * 1024 * 1024:  # 10 MB
+            raise ValidationError("File size must not be greater than 10 MB.")
+        return value
